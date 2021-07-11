726cd32f0fe56c74e951e61a9a3cda4328b9b74c
Deshmukh, Krantisinh <krantisinh.deshmukh@emc.com>
2021-07-11 11:48:37 +0530

    Rendered ArticleList from server side initially.

--- src/server/server.js
+++ src/server/server.js
@@ -1,20 +1,18 @@
 import express from "express";
-// import React from "react";
-// import ReactDOMServer from "react-dom/server";
-// import App from "../app";
 import { port } from "./config";
 import path from "path";
+import { serverRender } from "../renderers/server";
 
 const server = express();
 server.use(express.static("dist"));
 
 server.set("views", path.join(__dirname, "views"));
-
 server.set("view engine", "ejs");
-// const initialMarkup = ReactDOMServer.renderToString(<App />);
+
+const initialContent = serverRender();
 
 server.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { initialContent });
 });
 
 server.listen(port, () => console.info(`Running on port: ${port}`));
