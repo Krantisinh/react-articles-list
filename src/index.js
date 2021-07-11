cb6764a73b1bdb73a72e874ad47d9030eb77b303
Deshmukh, Krantisinh <krantisinh.deshmukh@emc.com>
2021-07-11 12:03:40 +0530

    Minor Refactoring

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./app";
-
-// ReactDOM.hydrate(<App></App>, document.getElementById("root"));
-
-ReactDOM.render(<App></App>, document.getElementById("root"));
